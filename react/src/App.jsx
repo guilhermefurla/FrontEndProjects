aa53516424bfea4d7134df18dd71d8592a53e2c7
Guilherme Furlanetti <165277271+guilhermefurla@users.noreply.github.com>
2024-08-18 15:37:15 -0300

    components

---
+++ react/src/App.jsx
@@ -0,0 +1,9 @@
+import { UserProfile } from "./components/UserProfile"
+export default function App(){
+  return(
+    <div>
+      <h1>Root component</h1>
+      <UserProfile/>
+    </div>
+  )
+}
---
+++ react/src/components/UserProfile.jsx
@@ -0,0 +1,20 @@
+export function UserProfile(){
+  return (
+    <div id="user-profile">
+      <p>Username: Bob</p>
+      <div>
+        <span>Email: </span>
+        <span>anson@ansonthedev.com</span>
+      </div>
+      <section>
+        <span>Favorite Foods:</span>
+        <br />
+        <ul>
+          <li>Sushi</li>
+          <li>Pizza</li>
+          <li>Rice</li>
+        </ul>
+      </section>
+    </div>
+  )
+}
