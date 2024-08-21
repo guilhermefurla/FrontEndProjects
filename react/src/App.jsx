48224f1e8a5c9331dbc8c6c1132d14e1a7cfc0a3
Guilherme Furlanetti <165277271+guilhermefurla@users.noreply.github.com>
2024-08-20 21:24:23 -0300

    type checking props

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,7 +3,7 @@ export default function App(){
   return(
     <div>
       <h1>Root component</h1>
-      <UserProfile age={20} isLoggedIn={true}/>
+      <UserProfile  username='bob' age={20} isLoggedIn={true}/>
     </div>
   )
 }
