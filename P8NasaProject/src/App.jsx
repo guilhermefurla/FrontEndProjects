332f250bd46053da9db632e25f38f27b51642d70
Guilherme Furlanetti <165277271+guilhermefurla@users.noreply.github.com>
2024-09-05 14:42:38 -0300

    showModal added

--- P8NasaProject/src/App.jsx
+++ P8NasaProject/src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 export default function App() {
+  const [showModal, setShowModal] = useState(false)
   return (
     <>
       <Main/>
-      <SideBar/>
+      {showModal && (
+        <SideBar/>
+      )}
       <Footer/>
     </>
   )
